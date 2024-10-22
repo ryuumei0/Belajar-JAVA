56a32311548b368309677a3d08f2016fe9315333
Ryuuu <168439957+ryuumei0@users.noreply.github.com>
2024-10-22 11:41:26 +0700

    update

---
+++ Belajar.Java/src/Java/Latihan_operator_logika.java
@@ -0,0 +1,18 @@
+package Java;
+import java.util.Scanner;
+
+public class Latihan_aritmatika {
+	
+	public static void main(String[] args) {
+	Scanner userInput = new Scanner(System.in);
+	
+	boolean hasil;
+	
+	System.out.print("masukang angkalebih dari 2 dan kurang dari 10 = ");
+	int a = userInput.nextInt();
+	
+	hasil = (a > 2) && (a < 10);
+	System.out.println("apakah sesuai ? " + hasil);
+	}
+}
+ 

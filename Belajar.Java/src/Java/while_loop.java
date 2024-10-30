6807c1bb9e8a3d4f51fa7a3bb6c2ba94b3ea249d
Ryuuu <168439957+ryuumei0@users.noreply.github.com>
2024-10-30 10:34:38 +0700

    update

--- Belajar.Java/src/Java/while_loop.java
+++ Belajar.Java/src/Java/while_loop.java
@@ -5,22 +5,32 @@ public class while_loop {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		int y, a;
+		int y, a, z, b;
 		
+		b = 1;
 		System.out.print("masukan jumlah segitiga = ");
 		a = input.nextInt();
 		
 		int x = 1;
 		while (x <= a ) {
-			y = x;
-			while (y <= a) {
+			y = 1;
+			while (y <= x) {
 				System.out.print(y + ", ");
 				y += 1;
 			}
 			System.out.println();
 			x += 1;
 		}
-		 
+		 x = 1;
+		while (a >= x ) {
+			y = a;
+			while (y >= x) {
+				System.out.print(y + ", ");
+				y -= 1;
+			}
+			System.out.println();
+			a -= 1;
+		}
 	}
 
 }

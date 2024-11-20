0746f249e7ea0f9ff1fbba3829949edaab76b5e7
Ryuuu <168439957+ryuumei0@users.noreply.github.com>
2024-11-20 09:55:05 +0700

    update

--- Belajar.Java/src/Java/kalimatriks.java
+++ Belajar.Java/src/Java/kalimatriks.java
@@ -2,40 +2,48 @@ package Java;
 
 public class kalimatriks {
     public static void main(String[] args) {
-        // Matriks pertama (2x3)
+
         int[][] array = {
-            {2, 4, 5},
-            {3, 5, 6},
-            {5, 7, 6}
-        
+            {2, 4},
+            {3, 5}
         };
-        
-        // Matriks kedua (3x2)
+
+
         int[][] ikan = {
-            {2, 3, 5},
-            {4, 5, 6},
-            {1, 3, 7}
+            {2, 3},
+            {4, 5}
         };
-        
-        // Matriks hasil perkalian (2x2)
-        int[][] hasil = new int[2][2];
-        
-        // Melakukan perkalian matriks
-        for (int i = 0; i < 2; i++) { // Mengulangi baris matriks pertama
-            for (int j = 0; j < 2; j++) { // Mengulangi kolom matriks kedua
-                for (int k = 0; k < 3; k++) { // Mengulangi elemen dalam baris pertama dan kolom kedua
-                    hasil[i][j] += array[i][k] * ikan[k][j]; // Rumus perkalian matriks
+
+
+        int baris_a = array.length;
+        int kolom_a = array[0].length;
+        int baris_b = ikan.length;
+        int kolom_b = ikan[0].length;
+
+
+        int [][] hasilkali = new int[baris_a][kolom_b];
+
+
+        for (int a = 0; a < baris_a; a++) {
+            for (int b = 0; b < kolom_b; b++) {
+                hasilkali[a][b] = 0;
+                for (int k = 0; k < kolom_a; k++) {
+                    hasilkali[a][b] += array[a][k] * ikan[k][b];
                 }
             }
         }
-        
-        // Menampilkan hasil perkalian
-        System.out.println("Matriks Hasil Perkalian:");
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                System.out.print(hasil[i][j] + " ");
+
+        printarray(hasilkali);
+    }
+
+
+    private static void printarray(int[][] data) {
+        for (int i = 0; i < data.length; i++) {
+            System.out.print("{");
+            for (int j = 0; j < data[i].length; j++) {
+                System.out.print(data[i][j] + " ");
             }
-            System.out.println();
+            System.out.println("}");
         }
     }
 }
